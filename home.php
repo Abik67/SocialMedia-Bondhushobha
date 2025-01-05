10a8c9ee9a3c28757615a242e34af30015814a10
Saiful Alam <107752127+Abik67@users.noreply.github.com>
2025-01-05 22:27:19 +0600

    1st

---
+++ home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to BondhuShobha</h1>
+    Already have an account? <a href="view/login.php">Login</a> <br>
+    Don't have an account? <a href="view/signup.php">Sign Up</a>
+</body>
+</html>
